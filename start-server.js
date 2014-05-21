683c50a97be507112c6cca43d11996758f9fb8d1
perfmjs <19589425@qq,cin>
2014-05-21 22:11:30 +0800

    重构node start服务

--- start-server.js
+++ start-server.js
@@ -3,4 +3,10 @@ perfmjs.app.newInstance();
 perfmjs.app.instance.register("pushserver", perfmjs.pushserver);
 perfmjs.app.instance.startAll();
 
-perfmjs.pushserver.instance.startServer();
+var i = 0;
+perfmjs.pushserver.instance.startServer(18000, "/jsbf", function(io_of_room) {
+    setInterval(function() {
+        i += 1;
+        io_of_room.emit("message", {"id":i});
+    }, 2000);
+});
